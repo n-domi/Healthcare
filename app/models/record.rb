aa82eca11e5ec67fdde16722b0e93c3d3566a08f
n-domi <59164751+n-domi@users.noreply.github.com>
2020-03-03 13:23:41 +0900

    Update record.rb

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,8 +1,11 @@
 class Record < ApplicationRecord
   belongs_to :user
+  has_many :goodfeeling, dependent: :destroy
+  has_many :badfeeling, dependent: :destroy
+  
   validates :content, {presence: true, length: {maximum: 140}}
   validates :user_id, {presence: true}
-  
+
   def user
     return User.find_by(id: self.user_id)
   end
